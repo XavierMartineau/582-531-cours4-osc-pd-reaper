3b58497fe3a032db8bf74d4406dd9f8ac53c0874
XavierMartineau <143350807+XavierMartineau@users.noreply.github.com>
2025-09-16 14:40:25 -0400

    feat: add LED control and OSC communication functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include <Arduino.h>
+#include <MicroOscSlip.h>
+#include <FastLED.h>
+CRGB pixelAtom;
 
-int myFunction();
+#define MA_BROCHE_BOUTON 39
+#define MA_BROCHE_ANGLE 32
+
+// Le nombre 128 entre les < > ci-dessous est le nombre maximal d'octets réservés pour les messages entrants.
+MicroOscSlip<128> monOsc(&Serial);
+
+// Déclaration d'un tableau de LEDs
 
 void setup()
 {
+  // Initialisation des LEDs
+  FastLED.addLeds<WS2812, 27, GRB>(&pixelAtom, 7);
+
+  pinMode(MA_BROCHE_BOUTON, INPUT);
+  Serial.begin(115200);
 }
 
 void loop()
 {
+  // Lecture de l'angle
+  int maLectureAngle = analogRead(MA_BROCHE_ANGLE);
+  monOsc.sendInt("/angle", maLectureAngle);
+
+  // Lecture du bouton
+  int maLectureBouton = digitalRead(MA_BROCHE_BOUTON);
+  monOsc.sendInt("/Bouton", maLectureBouton);
+
+  // Animation simple : couleur verte qui change avec le temps
+  pixelAtom = CRGB(0, millis() % 256, 0); // Correction ici : utiliser pixelAtom, pas pixels
+  FastLED.show();
+
+  delay(100); // Petite pause pour limiter la fréquence
 }
